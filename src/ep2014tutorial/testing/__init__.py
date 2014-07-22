60390007b0f84c36e6153275d289caafb72c1246
dobe <bernddorn@gmail.com>
2014-07-22 17:29:16 +0200

    first test

---
+++ src/ep2014tutorial/testing/__init__.py
@@ -0,0 +1,12 @@
+import os
+from crate.testing.layer import CrateLayer
+
+project_root = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+def crate_path(*parts):
+    return os.path.join(project_root, 'parts', 'crate', *parts)
+
+crate_layer = CrateLayer('crate',
+                         crate_home=crate_path(),
+                         port=44200)
--- src/ep2014tutorial/tests.py
+++ src/ep2014tutorial/tests.py
@@ -1,13 +1,19 @@
 import os
 import unittest
+import doctest
 from webtest import TestApp
 from pyramid import paster
+from . import testing
+from . import model
 
 here = os.path.dirname(__file__)
 test_dir = os.path.join(here, 'testing')
 conf = os.path.join(test_dir, 'testing.ini')
 
+schema_sql = os.path.join(here, 'schema.sql')
+
 default_app = None
+
 def get_app():
     global default_app, app
     if default_app is None:
@@ -18,5 +24,28 @@ def get_app():
 def setUp(test, app_func=get_app):
     app = app_func()
     testapp = TestApp(app)
-    test.globs['browser'] = testapp
+    test.globs['app'] = testapp
 
+    with open(schema_sql) as f:
+        model.DBSession.execute(f.read().split(';')[0])
+
+def create_suite(testfile, layer=None,
+                 optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS,
+                 level=None,
+                 **kwargs):
+    s = doctest.DocFileSuite(
+        testfile, setUp=setUp,
+        optionflags=optionflags,
+        **kwargs
+        )
+    if layer:
+        s.layer = layer
+    if level:
+        s.level = level
+    return s
+
+def test_suite():
+    suites = (
+        create_suite('probe_status.rst', layer=testing.crate_layer),
+        )
+    return unittest.TestSuite(suites)
