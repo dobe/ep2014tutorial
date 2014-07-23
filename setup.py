e91c16e2adc72057b60874c7b422c602156450d4
dobe <bernddorn@gmail.com>
2014-07-23 15:25:36 +0200

    entry points added

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ setup(
     url='https://github.com/dobe/ep2014tutorial',
     package_dir={'': 'src'},
     packages=find_packages('src'),
+    entry_points={
+        'paste.app_factory': [
+            'main=ep2014tutorial.server:app_factory',
+            ],
+        'console_scripts': [
+            'app=pyramid.scripts.pserve:main',
+            ],
+          },
     extras_require=dict(
         test=['crate[test]',
               'webtest'
